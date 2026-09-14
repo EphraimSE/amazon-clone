a3a275097f0d8fdf715c91f5fbfd23bcb310e24c
Ephraim Seoke <ephraim.seoke@gmail.com>
2026-09-14 21:57:25 +0200

    products component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import './App.css';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import "./App.css";
+import Products from "./components/Products";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-    
-   
-    </>
-  )
+    <div className="app">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </Router>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
