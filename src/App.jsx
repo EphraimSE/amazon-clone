3ebbaa4589cdbcab3cf7cda92486d89de399aa92
Ephraim Seoke <ephraim.seoke@gmail.com>
2026-09-15 08:21:17 +0200

    add product details page and register route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
-import Products from "./components/Products";
+import Products from "./pages/Products";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -11,11 +12,12 @@ function App() {
     <div className="app">
       <Router>
         <Header />
-        <Navbar />
         <main>
+          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/product-details/:id" element={<ProductDetails />} />
           </Routes>
         </main>
       </Router>
