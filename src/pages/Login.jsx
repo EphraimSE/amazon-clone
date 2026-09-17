54d0f68995f62bd33fe41bf062b6d69b30e6e0ae
Ephraim Seoke <ephraim.seoke@gmail.com>
2026-09-17 07:03:26 +0200

    add useRef hook to read user sign in page input data

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import './Login.css';
 
 const Login = () => {
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  /**use variables */
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+  // const emailChangeHandler = (e) => {
+  //   setEmail(e.target.value);
+  // }
+
+  // const passwordChangeHandler = (e) => {
+  //   setPassword(e.target.value);
+  // }
+
+  // prevent default form submit behavior
+  const signIn = e => {
+    e.preventDefault();
+    const enteredEmail = (emailRef.current.value);
+    const enteredPassword = (passwordRef.current.value);
+
+     console.log('Email:', enteredEmail + " Password:", enteredPassword);
+  };
+
+  
+
   return (
     <div className="login">
       <Link to="/">
         <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" 
-        ALT="Amazon Page"
+        alt="Amazon logo"
         className="login_logo"
         />
       </Link>
       <div className="login_container">
         <h1>sign in or create account</h1>
         <form>
-          <h5>Enter mobile number or email</h5>
-          <input type="text"/>
-          <button type="submit" className="login_signInButton">Continue</button>
+          <h5>Enter mobile number or email Address</h5>
+          <input type="text" ref={emailRef}
+          alt="Mobile or email address"
+          />
+          <h5>Password</h5>
+          <input type="password" ref={passwordRef}
+          alt="password"
+          />
+
+          <button type="submit" className="login_signInButton" onClick={signIn}>Continue</button>
         </form>
         <p>By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
         <p>Need Help?</p>
