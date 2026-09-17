1b1ed9e884b1a229d63c731a6f9d051966c93553
Ephraim Seoke <ephraim.seoke@gmail.com>
2026-09-17 13:49:46 +0200

    clean up login page code, destructure state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,58 @@
-import React, { useState, useReducer} from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
-
 // useReducer hook
 const reducer = (state, action) => {
-  if(action.type === "EMAIL_INPUT") {
-    return {...state, emailValue: action.payload}
+  if (action.type === "EMAIL_INPUT") {
+    return { ...state, emailValue: action.payload };
   }
-    if(action.type === "PASSWORD_INPUT") {
-    return {...state, passwordValue: action.payload}
+  if (action.type === "PASSWORD_INPUT") {
+    return { ...state, passwordValue: action.payload };
   }
-
-
-  return{ emailValue: "", passwordValue:"" } 
+  return { emailValue: "", passwordValue: "" };
 };
+
 const Login = () => {
-  
-  //const [formIsValid, setFormIsValid] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
 
   // initialize reducer
   const [state, dispatch] = useReducer(reducer, {
     emailValue: "",
-    passwordValue:"",
-
+    passwordValue: "",
   });
-   // validate email contains @ symbol
+
+  // desctructure state
+  const {emailValue: email, passwordValue: password} = state;
+
+  // useEffect
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("CHECKING FORM VALIDITY");
+    setFormIsValid(
+      email.includes("@)") && password.trim().length > 6);
+    }, 500);
+  return() => {
+    console.log("CLEANUP HERE");
+    clearTimeout(identifier);
+  };
+}, [email, password]);
+
+  // validate email contains @ symbol
   const emailChangeHandler = (e) => {
-    dispatch({type: "EMAIL_INPUT", payload: e.target.value})
-    //setEmail(e.target.value);
-    //setFormIsValid(email.includes("@") && password.trim().length > 6);
+    dispatch({ type: "EMAIL_INPUT", payload: e.target.value });
   };
 
   // validate password contains 6 or more characters
   const passwordChangeHandler = (e) => {
-   // setPassword(e.target.value);
-    dispatch({type: "PASSWORD_INPUT", payload: e.target.value})
-    // setFormIsValid(email.includes("@") && e.target.value.trim().length > 6);
+    dispatch({ type: "PASSWORD_INPUT", payload: e.target.value });
+  };
 
-};
-  
   // prevent default form submit behavior
   const signIn = (e) => {
     e.preventDefault();
-    console.log("Entered email: ", state.emailValue)
-    console.log("Entered password", state.passwordValue)
+    console.log("Entered email: ", email);
+    console.log("Entered password", password);
   };
 
   return (
@@ -60,14 +68,16 @@ const Login = () => {
         <h1>sign in or create account</h1>
         <form>
           <h5>Enter mobile number or email Address</h5>
-          <input type="text" 
-          value={state.emailValue} 
-          onChange={emailChangeHandler}
+          <input
+            type="text"
+            value={email}
+            onChange={emailChangeHandler}
           />
           <h5>Password</h5>
-          <input type="password" 
-          value={state.passwordValue}
-          onChange={passwordChangeHandler}
+          <input
+            type="password"
+            value={password}
+            onChange={passwordChangeHandler}
           />
 
           <button type="submit" className="login_signInButton" onClick={signIn}>
@@ -87,6 +97,6 @@ const Login = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
