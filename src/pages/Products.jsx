c16e6edd7f488acf94cd017d8448a5fb59e175a2
Ephraim Seoke <ephraim.seoke@gmail.com>
2026-09-24 12:57:21 +0200

    remove newly added dummy product Link

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -14,9 +14,6 @@ const Products = () => {
           <li>
             <Link to ="/products/03">Product 3</Link>
           </li>
-          <li>
-            <Link to ="/products/04">Product 4</Link>
-          </li>
         </ul>
     </div>
   )
